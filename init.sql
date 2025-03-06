ee27ed441877abe9b1300b19ac8533f53ff1475e
Kirill <kirillfromhome@gmail.com>
2025-03-06 07:59:57 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,7 +1,17 @@
 CREATE DATABASE my_postgres;
 
--- Создание пользователя и предоставление прав
-CREATE USER test_user WITH PASSWORD 'test1234';
+-- Проверка существования пользователя и назначение пароля
+DO $$
+BEGIN
+    -- Проверяем, существует ли пользователь test_user
+    IF EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'test_user') THEN
+        -- Если пользователь существует, назначаем ему пароль
+        ALTER USER test_user WITH PASSWORD 'test1234';
+    ELSE
+        -- Если пользователь не существует, создаем его и назначаем пароль
+        CREATE USER test_user WITH PASSWORD 'test1234';
+    END IF;
+END $$;
+
+-- Предоставление прав на базу данных пользователю
 GRANT ALL PRIVILEGES ON DATABASE my_postgres TO test_user;
-
-
